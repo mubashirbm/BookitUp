ce8bd0396216649170b8dc2cff3b6bda908bdd40
mubashirbm <104245914+mubashirbm@users.noreply.github.com>
2023-02-25 08:26:59 +0530

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      // target: 'http://localhost:5000',
+      target: 'https://api.fitzone.fun',
       changeOrigin: true,
     })
   );
