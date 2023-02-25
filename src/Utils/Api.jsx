4946c114bca182a62d629d0e3ae34efd37010631
mubashirbm <104245914+mubashirbm@users.noreply.github.com>
2023-02-25 09:11:56 +0530

    fgfdgf

--- src/Utils/Api.jsx
+++ src/Utils/Api.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 
-export const userApi = axios.create({ baseURL:"http://localhost:5000/api" });
-// export const userApi = axios.create({ baseURL:"http://api.fitzone.fun/api" });
+// export const userApi = axios.create({ baseURL:"http://localhost:5000/api" });
+export const userApi = axios.create({ baseURL:"http://api.fitzone.fun/api" });
 
-// export const adminAPI = axios.create({ baseURL:"http://api.fitzone.fun/admin" });
-export const adminAPI = axios.create({ baseURL:"http://localhost:5000/admin" });
+export const adminAPI = axios.create({ baseURL:"http://api.fitzone.fun/admin" });
+// export const adminAPI = axios.create({ baseURL:"http://localhost:5000/admin" });
 
